708573564a04a16f28d160557093b1fe6e3850a2
CsharpMaster37 <misha310303@gmail.com>
2024-10-12 22:20:20 +0300

    Улучшил стилизацию элементов квадрата, написал функцию проверки начисления бонусов.

--- client/src/components/ModalWindow_Square/ModalWindow_Square.jsx
+++ client/src/components/ModalWindow_Square/ModalWindow_Square.jsx
@@ -2,29 +2,39 @@ import classes from './modalWindow_Square.module.css'
 import { useState, useEffect } from 'react'
 
 const ModalWindow_Square = (props) => {
-    const { active, setActive, numberQuestion, isCloseQuestions, setisCloseQuestions } = props;
+    const { active, setActive, numberQuestion, isCloseQuestions, setisCloseQuestions, score, setScore } = props;
     const [time, setTime] = useState(120);
-
+    const checkCloseQuestions = isCloseQuestions[numberQuestion - 1]
     useEffect(() => {
         time > 0 ? setTimeout(() => setTime(time - 1), 1000) : handleAnswer();
     }, [time]);
 
 
     const handleAnswer = () => {
-        if (isCloseQuestions[numberQuestion - 1] === undefined) {
+        if (checkCloseQuestions === undefined) {
             //#region Обработки ответа
 
             //#endregion
+
+            //#region development
+            CloseQuestion(true)
+            ChangeScore(numberQuestion)
+            //#endregion
+
             setActive(false)
         }
     }
     const handleSurrender = () => {
         CloseQuestion(false)
+        ChangeScore(-numberQuestion)
         setActive(false)
     }
     const CloseQuestion = (status) => {
         isCloseQuestions[numberQuestion - 1] = status
         setisCloseQuestions(isCloseQuestions)
+    }
+    const ChangeScore = (number) => {
+        setScore(score + (number * 10))
     }
 
     return (
@@ -34,17 +44,19 @@ const ModalWindow_Square = (props) => {
                     some content
                 </div>
                 <div className={classes.content_answer}>
-                    <textarea defaultValue='some text' className={classes.text_answer} />
-                    {isCloseQuestions[numberQuestion - 1] === undefined ?
+                    {checkCloseQuestions === undefined ?
+                        <textarea defaultValue='' placeholder='Введите ответ:' className={classes.text_answer} /> :
+                        <textarea defaultValue='your answer' readOnly className={classes.text_answer} />}
+                    {checkCloseQuestions === undefined ?
                         <span className={classes.time}>{time}</span> :
-                        isCloseQuestions[numberQuestion - 1] ?
+                        checkCloseQuestions ?
                             <span className={classes.time}>&#9989;</span> :
                             <span className={classes.time}>&#10060;</span>
                     }
                     <div className={classes.content_buttons}>
-                        {isCloseQuestions[numberQuestion - 1] === undefined ?
+                        {checkCloseQuestions === undefined ?
                             <><button className={`${classes.button} ${classes.green}`} onClick={() => handleAnswer()}>Ответить</button>
-                                <button className={`${classes.button} ${classes.red}`} onClick={() => handleSurrender()}>Сдаться (-500)</button></> :
+                                <button className={`${classes.button} ${classes.red}`} onClick={() => handleSurrender()}>Сдаться (-{numberQuestion * 10})</button></> :
                             <button className={`${classes.button} ${classes.red}`} onClick={() => setActive(false)}>Закрыть</button>}
                     </div>
                 </div>
--- client/src/pages/SquareGame/SquareGame.jsx
+++ client/src/pages/SquareGame/SquareGame.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import GameInformationPanel from './../../components/GameInformationPanel/GameInformationPanel'
 import QuestionButton from '../../components/QuestionButton/QuestionButton'
 import ModalWindow_Square from '../../components/ModalWindow_Square/ModalWindow_Square'
 import classes from './squaregame.module.css'
+import useDidMountEffect from './../../customHooks/useDidMountEffect'
 
 const SquareGame = (props) => {
     let { levels, themes } = props
     const [modalActive, setModalActive] = useState(false)
     const [numberQuestion, setNumberQuestion] = useState(0)
     const [isCloseQuestions, setisCloseQuestions] = useState(new Array(25).fill(undefined))
+    const [bonusRow, setBonusRow] = useState(new Array(5).fill(undefined))
+    const [bonusCol, setBonusCol] = useState(new Array(5).fill(undefined))
+    const [score, setScore] = useState(0)
+    useDidMountEffect(() => {
+        if (isCloseQuestions[numberQuestion - 1] !== undefined) {
+            const indexRow = (numberQuestion - (numberQuestion % 5)) / 5
+            const indexCol = numberQuestion % 5 - 1
+            if (bonusRow[indexRow] === undefined) {
+                bonusRow[indexRow] = true;
+                for (let i = 0; i < 5; i++) {
+                    if (isCloseQuestions[indexRow * 5 + i] === false) {
+                        bonusRow[indexRow] = false;
+                        break;
+                    }
+                }
+                setBonusRow(bonusRow)
+            }
+            if (bonusCol[indexCol] === undefined) {
+                bonusCol[indexCol] = true;
+                for (let i = 0; i < 5; i++) {
+                    if (isCloseQuestions[i * 5 + indexCol] === false) {
+                        bonusCol[indexCol] = false;
+                        break;
+                    }
+                }
+                setBonusCol(bonusCol)
+            }
+        }
+    }, [isCloseQuestions[numberQuestion - 1]])
     //#region development
     levels = [];
     for (let i = 1; i <= 5; i++) {
@@ -23,11 +53,12 @@ const SquareGame = (props) => {
         <main className="section">
             <div className="container">
                 <div className={classes.wrapper}>
-                    <GameInformationPanel />
+                    <GameInformationPanel score={score} />
                     <ModalWindow_Square key={numberQuestion}
                         active={modalActive} setActive={setModalActive}
                         numberQuestion={numberQuestion}
-                        isCloseQuestions={isCloseQuestions} setisCloseQuestions={setisCloseQuestions} />
+                        isCloseQuestions={isCloseQuestions} setisCloseQuestions={setisCloseQuestions}
+                        score={score} setScore={setScore} />
                     <table className={classes.table_square}>
                         <thead>
                             <tr className={classes.tr_square}>
@@ -47,7 +78,7 @@ const SquareGame = (props) => {
                                             numberQuestion={themeNumber * 5 + level}
                                             setNumberQuestion={setNumberQuestion} isCloseQuestions={isCloseQuestions[themeNumber * 5 + level - 1]} />
                                     ))}
-                                    <td className={`${classes.bonus} ${classes.td_square}`}>+{(themeNumber) * 10}</td>
+                                    <td className={`${classes.bonus} ${classes.td_square}`}>+{(themeNumber + 1) * 10}</td>
                                 </tr>
                             ))}
                             <tr className={classes.tr_square}>
