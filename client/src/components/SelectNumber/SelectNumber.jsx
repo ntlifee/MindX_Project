a26da307d27c0966c6b5e1b5de4092fb415c40b7
ntlifee <perfilov.mv@yandex.ru>
2024-11-08 19:17:26 +0300

    Отслеживание изменений scoreFirstQuestion, scoreSuccess, scoreFailure

--- client/src/components/SelectNumber/SelectNumber.jsx
+++ client/src/components/SelectNumber/SelectNumber.jsx
@@ -1,12 +1,22 @@
 import classes from './selectnumber.module.css'
 
 const SelectNumber = (prop) => {
-    const { Name } = prop;
+    const { Name, Value, Min, Action } = prop;
     return (
-        <div className={classes.wrapper}>
+        <>
             <label for={Name}>{Name}</label>
-            <input id={Name} min={0} type="number" ></input>
-        </div>
+            <input
+                id={Name}
+                className={classes.input}
+                type="number"
+                value={Value}
+                min={Min}
+                onChange={event => {
+                    const newValue = event.target.value
+                    Action && Action(newValue)
+                }}
+            />
+        </>
     );
 }
 
--- client/src/pages/CarouselGame/CreateOrUpdateGame/CarouselCreateOrUpdate.jsx
+++ client/src/pages/CarouselGame/CreateOrUpdateGame/CarouselCreateOrUpdate.jsx
@@ -1,14 +1,22 @@
 import classes from './carouselcreteorupdate.module.css'
 import SelectNumber from '../../../components/SelectNumber/SelectNumber';
 
+import { useState } from 'react'
+
 const CarouselCreateOrUpdate = () => {
+    const [scoreFirstQuestion, setScoreFirstQuestion] = useState(1)
+    const [scoreSuccess, setScoreSuccess] = useState(1)
+    const [scoreFailure, setScoreFailure] = useState(0)
     return (
         <main className={classes.section}>
             <div className="container">
                 <div className={classes.wrapper}>
-                    <SelectNumber Name={'Балл первого вопроса:'} />
-                    <SelectNumber Name={'Получение за правильный ответ:'} />
-                    <SelectNumber Name={'Снятие за неправильный ответ:'} />
+                    <h1 className={classes.title}>Создание и редактирование игры</h1>
+                    <div className={classes.select}>
+                        <SelectNumber Name={'Балл первого вопроса:'} Value={scoreFirstQuestion} Min={1} Action={setScoreFirstQuestion} />
+                        <SelectNumber Name={'Получение за правильный ответ:'} Value={scoreSuccess} Min={1} Action={setScoreSuccess} />
+                        <SelectNumber Name={'Снятие за неправильный ответ:'} Value={scoreFailure} Min={0} Action={setScoreFailure} />
+                    </div>
 
                 </div>
             </div>
