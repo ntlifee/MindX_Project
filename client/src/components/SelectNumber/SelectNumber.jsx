155409a5e2c6ea3125a4d3c13c50f11df9bec5f1
ntlifee <perfilov.mv@yandex.ru>
2024-11-09 12:21:54 +0300

    Изменения редактирования данных игры под мобильный интерфейс

--- client/src/components/SelectNumber/SelectNumber.jsx
+++ client/src/components/SelectNumber/SelectNumber.jsx
@@ -1,22 +1,24 @@
 import classes from './selectnumber.module.css'
 
 const SelectNumber = (prop) => {
-    const { Name, Value, Min, Action } = prop;
+    const { Id, Name, Value, Min, Action } = prop;
     return (
-        <>
-            <label for={Name}>{Name}</label>
+        <div className={classes.wrapper}>
+            <label htmlFor={Id}>{Name}</label>
             <input
-                id={Name}
+                id={Id}
                 className={classes.input}
                 type="number"
                 value={Value}
-                min={Min}
                 onChange={event => {
-                    const newValue = event.target.value
+                    let newValue = event.target.value
+                    if (newValue !== '') {
+                        newValue = Math.max(parseInt(newValue), Min);
+                    }
                     Action && Action(newValue)
                 }}
             />
-        </>
+        </div>
     );
 }
 
--- client/src/pages/CarouselGame/CreateOrUpdateGame/CarouselCreateOrUpdate.jsx
+++ client/src/pages/CarouselGame/CreateOrUpdateGame/CarouselCreateOrUpdate.jsx
@@ -6,16 +6,16 @@ import { useState } from 'react'
 const CarouselCreateOrUpdate = () => {
     const [scoreFirstQuestion, setScoreFirstQuestion] = useState(1)
     const [scoreSuccess, setScoreSuccess] = useState(1)
-    const [scoreFailure, setScoreFailure] = useState(0)
+    const [scoreFailure, setScoreFailure] = useState(1)
     return (
         <main className={classes.section}>
             <div className="container">
                 <div className={classes.wrapper}>
                     <h1 className={classes.title}>Создание и редактирование игры</h1>
                     <div className={classes.select}>
-                        <SelectNumber Name={'Балл первого вопроса:'} Value={scoreFirstQuestion} Min={1} Action={setScoreFirstQuestion} />
-                        <SelectNumber Name={'Получение за правильный ответ:'} Value={scoreSuccess} Min={1} Action={setScoreSuccess} />
-                        <SelectNumber Name={'Снятие за неправильный ответ:'} Value={scoreFailure} Min={0} Action={setScoreFailure} />
+                        <SelectNumber Id={classes.select1} Name={'Первый вопрос:'} Value={scoreFirstQuestion} Min={1} Action={setScoreFirstQuestion} />
+                        <SelectNumber Id={'select2'} Name={'Правильный ответ: +'} Value={scoreSuccess} Min={1} Action={setScoreSuccess} />
+                        <SelectNumber Id={'select3'} Name={'Неправильный ответ: -'} Value={scoreFailure} Min={1} Action={setScoreFailure} />
                     </div>
 
                 </div>
