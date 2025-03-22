67a29f9b2275480928a8b81368a752d61b3f878f
ntlifee <perfilov.mv@yandex.ru>
2025-03-22 23:40:04 +0300

    Изменеие схемы для роли и темы

--- server/schemas/roleAndThemeSchema.js
+++ server/schemas/roleAndThemeSchema.js
@@ -2,12 +2,10 @@ const Joi = require("joi");
 
 const schema = Joi.object({
     name: Joi.string()
-        .empty()
-        .pattern(/^[a-zA-Z0-9А-Яа-яЁё ]+$/)
-        .min(3)
+        .pattern(/^[a-zA-Z0-9А-Яа-яЁё -]+$/)
+        .min(1)
         .max(30)
         .messages({
-            "string.empty": "Поле 'name' не может быть пустым",
             "string.base": "Поле 'name' должно быть строкой",
             "string.pattern.base": "Поле 'name' должно содержать только буквы и цифры",
             "string.min": "Поле 'name' должно содержать не менее 3 символов",
