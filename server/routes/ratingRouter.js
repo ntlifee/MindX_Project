c08c6016de72f8208bf17f88bb3bb385a04e48f5
ntlifee <perfilov.mv@yandex.ru>
2025-05-18 21:42:00 +0300

    Добавлена ручка

--- server/routes/ratingRouter.js
+++ server/routes/ratingRouter.js
@@ -1,7 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const ratingController = require('../controllers/ratingController')
+const gameController = require('../controllers/gameController')
 
+router.get('/', gameController.getAllUser)
 router.get('/:id', ratingController.getOne.bind(ratingController))
 
 module.exports = router
