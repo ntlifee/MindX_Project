e98786eb05fb60dcb794bd032ed40a84d43da1e7
ntlifee <perfilov.mv@yandex.ru>
2025-05-24 15:54:10 +0300

    Логирование и poll подключений бд

--- server/database.js
+++ server/database.js
@@ -7,6 +7,14 @@ module.exports = new Sequelize(
     {
         dialect: 'postgres',
         host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT
-    }
+        port: process.env.DATABASE_PORT,
+        logging: process.env.DATABASE_LOGGING === 'true',
+        benchmark: true, //логирование времени выполнения запросов
+        pool: {
+            max: 20,
+            min: 5,       // поддерживать 5 активных соединений всегда
+            acquire: 5000, // 5 секунд ожидания
+            idle: 30000    // 30 секунд жизни неиспользуемого соединения
+        }
+    },
 )
