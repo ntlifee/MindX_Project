1e028047b12ede15305286b6c1b9a20c391091b3
ntlifee <perfilov.mv@yandex.ru>
2025-05-25 23:48:23 +0300

    Убран replace

--- server/middlewares/ErrorHandlingMiddleware.js
+++ server/middlewares/ErrorHandlingMiddleware.js
@@ -14,7 +14,7 @@ module.exports = function (err, req, res, next) {
         })
     }
     if (err instanceof ApiError) {
-        return res.status(err.status).json({ error: err.message.replace(/"/g, '\'') })
+        return res.status(err.status).json({ error: err.message })
     }
     return res.status(500).json({ error: "Непредвиденная ошибка!" })
 }
